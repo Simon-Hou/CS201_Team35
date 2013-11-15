3a754ced65a9d7c434fd714421c420b60b157de4
Gabriel Mel de Fontenay <meldefon@usc.edu>
2013-11-14 18:44:12 -0800

    Added agent code and basic role code that will give us the role objects we need

---
+++ src/role/Role.java
@@ -0,0 +1,15 @@
+package role;
+
+import agent.Agent;
+
+public abstract class Role extends Agent{
+
+	
+	public synchronized void startThread() {
+        
+		//Don't do anything! This is a role.
+		
+    }
+	
+	
+}
