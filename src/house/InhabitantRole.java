935b01ae13542239caa53b699bbe798279f25fd5
Simon Hou <fangliho@usc.edu>
2013-11-17 22:28:52 -0800

    basic (empty)files created

---
+++ src/house/InhabitantRole.java
@@ -0,0 +1,14 @@
+package house;
+
+import interfaces.Inhabitant;
+import role.Role;
+
+public class InhabitantRole extends Role implements Inhabitant {
+
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
