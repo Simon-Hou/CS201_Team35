ca4d9567a4d6de2f579565b4c182ba00e909834a
Parker <pqseagren@sbcglobal.net>
2013-11-21 20:04:25 -0800

    making marketPanel

--- src/market/gui/MarketMain.java
+++ src/market/gui/MarketMain.java
@@ -1,5 +1,40 @@
 package market.gui;
 
-public class MarketMain {
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
+
+public class MarketMain extends JFrame{
+	
+
+	
+	MarketMain(){
+	    int WINDOWX = 500;
+	    int WINDOWY = 500;	
+		setBounds(50, 50, WINDOWX,WINDOWY);
+		setLayout(new FlowLayout());
+		
+		MarketPanel mp = new MarketPanel();
+        Dimension mpSize = new Dimension (WINDOWX, WINDOWY);
+        mp.setPreferredSize(mpSize);
+        mp.setMinimumSize(mpSize);
+        mp.setMaximumSize(mpSize);	
+		add(mp);
+		
+		
+	}
+
+	public static void main(String[] args) {
+
+		MarketMain gui = new MarketMain();
+        gui.setTitle("Market Panel");
+        gui.setVisible(true);
+        gui.setResizable(true);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
--- src/market/gui/MarketPanel.java
+++ src/market/gui/MarketPanel.java
@@ -1,6 +1,11 @@
 package market.gui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -9,40 +14,125 @@ import java.util.List;
 import javax.swing.*;
 
 
+
+
 public class MarketPanel extends JPanel implements ActionListener{
 
 	private JLabel title = new JLabel("Market");
-	private JPanel inventoryPanel = new JPanel ();
-	private List<JButton> inventoryList = new ArrayList<JButton>();
+	private List<InventoryItem> inventoryList = new ArrayList<InventoryItem>();
 	public JScrollPane pane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 								JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     private JPanel view = new JPanel();
     
-    private JButton button1 = new JButton("Steak");
-	
+
+    private InventoryItem button1 = new InventoryItem("Steak", this);
+    private InventoryItem button2 = new InventoryItem("Chicken", this);
 	
 	public MarketPanel(){
-		pane.setViewportView(view);
-		add(title);
-		add(inventoryPanel);
 		
-		 Dimension paneSize = pane.getSize();
-         Dimension buttonSize = new Dimension(paneSize.width - 20,
-                 (int) (paneSize.height / 3));
+        setLayout(new FlowLayout());
+		add(title);
+		
+		view.setLayout(new BoxLayout((Container) view, BoxLayout.Y_AXIS));
+		pane.setViewportView(view);
+        add(pane);
+		
+        //^^^^^^^^^^  good. now to add the button. make new class that has (-, name, invent, +)
+       
+      
+       // button1.setBackground(Color.white);
+   //button1.setForeground(Color.white);
+        
+        
+       
+        Dimension paneSize = new Dimension (500,300);
+        pane.setPreferredSize(paneSize);
+        pane.setMinimumSize(paneSize);
+        pane.setMaximumSize(paneSize);
+        
+      
+
+         Dimension buttonSize = new Dimension(paneSize.width-20, (int) (paneSize.height / 10));
+         
          button1.setPreferredSize(buttonSize);
          button1.setMinimumSize(buttonSize);
          button1.setMaximumSize(buttonSize);
-         button1.addActionListener(this);
          inventoryList.add(button1);
          view.add(button1);
+         
+         
+         button2.setPreferredSize(buttonSize);
+         button2.setMinimumSize(buttonSize);
+         button2.setMaximumSize(buttonSize);
+         inventoryList.add(button2);
+         view.add(button2);
 		
+         validate();
+         
+        	 
+         
 		
 	}
 	
 	
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		System.err.println("Something was pressed.");
+		
+		for (InventoryItem item : inventoryList){
+			if (e.getSource() == item.minus && item.inventory >0){
+				//System.err.println("Minus button pressed.");
+				item.inventory--;
+				item.inventoryLabel.setText(item.inventory.toString());
+			}
+			else if (e.getSource() == item.plus){
+				//System.err.println("Plus button pressed.");
+				item.inventory++;
+				item.inventoryLabel.setText(item.inventory.toString());
+			}
+		}
 		
 	}
 
+	//Utilities
+	
+	private class InventoryItem extends JPanel{
+		
+		MarketPanel mp;
+		
+		String choice;
+		Integer inventory;
+		
+		JButton minus = new JButton("-");
+		JLabel choiceLabel = new JLabel();
+		JLabel inventoryLabel = new JLabel();
+		JButton plus = new JButton("+");
+		
+		InventoryItem(String name, MarketPanel mp){
+			this.mp = mp;
+			choice = name;
+			inventory = 0;
+			
+			setLayout(new GridLayout(1,5,5,5));
+			setBorder(BorderFactory.createRaisedBevelBorder());
+			
+			
+			choiceLabel.setText(choice);
+			inventoryLabel.setText(inventory.toString());
+		
+			//choiceLabel.setHorizontalTextPosition(JLabel.RIGHT);
+			//inventoryLabel.setVerticalTextPosition(SwingConstants.CENTER);
+		
+	        // button1.addActionListener(this);
+			minus.addActionListener(mp);
+			plus.addActionListener(mp);
+			
+			
+			add(minus);
+			add(choiceLabel);
+			add(inventoryLabel);
+			add(plus);
+			
+		}
+	}
+	
 }
