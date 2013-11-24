eec3bb50889ad00dd3c992b29c4a86a22df71d45
Linda Xu <lindaxu@usc.edu>
2013-11-24 14:46:54 -0800

    Renamed CookAgent. Implemented the ProducerConsumer waiter and also updated cook. Close #61. Close #62. Close #63.

--- src/restaurant/restaurantLinda/gui/CookGui.java
+++ src/restaurant/restaurantLinda/gui/CookGui.java
@@ -1,6 +1,6 @@
 package restaurant.restaurantLinda.gui;
 
-import restaurant.restaurantLinda.CookAgent;
+import restaurant.restaurantLinda.CookRole;
 import restaurant.restaurantLinda.WaiterRole;
 
 import java.awt.*;
@@ -16,7 +16,7 @@ public class CookGui implements Gui {
 	public static final int WINDOWX = AnimationPanel.WINDOWX;		//Same as animation panel
     public static final int WINDOWY = AnimationPanel.WINDOWY;
 	
-	CookAgent agent;
+	CookRole agent;
 	
 
 	private int personSize=AnimationPanel.PERSONSIZE;
@@ -34,7 +34,7 @@ public class CookGui implements Gui {
 	private String bufferText;
 	public final String path="../../images/";
 
-    public CookGui(CookAgent agent) {
+    public CookGui(CookRole agent) {
     	this.agent = agent;
     	
     	xPos = xDestination = home.x;
