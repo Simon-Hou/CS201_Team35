4b960431aa273c2db87ff67e58c7cbed397849de
rgroom <rgroom@usc.edu>
2013-11-26 12:58:03 -0800

    working on crashes

--- src/cityGui/SimCityGui.java
+++ src/cityGui/SimCityGui.java
@@ -337,14 +337,14 @@ public class SimCityGui extends JFrame implements ActionListener {
 		test.addBuses(test);
 		test.addNewBuilding("Bank", 5, 400);
 		test.addNewBuilding("Market",200,250);
-		test.addNewBuilding("Restaurant", 5, 200);
+		//test.addNewBuilding("Restaurant", 5, 200);
 		//test.addNewBuilding("Market", 250, 200);
 		test.addNewBuilding("House", 200, 5);
 		//test.addNewBuilding("House", 500, 5);
 		test.addBuses(test);
 		test.fullyManBuilding("Bank",0);
 		test.fullyManBuilding("Market",0);
-		test.fullyManBuilding("Restaurant",0);
+		//test.fullyManBuilding("Restaurant",0);
 		//test.fullyManBuilding("Market",1);
 		//test.fullyManBuilding("Bank",0);
 		//test.fullyManBuilding("Market",0);
