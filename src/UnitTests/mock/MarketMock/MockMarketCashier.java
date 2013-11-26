73c508280b734c28d1f753471a06b4e54571a6ee
Parker <pqseagren@sbcglobal.net>
2013-11-25 22:29:04 -0800

    beginning market unit testing

--- src/UnitTests/mock/MarketMock/MockMarketCashier.java
+++ src/UnitTests/mock/MarketMock/MockMarketCashier.java
@@ -1,5 +1,76 @@
 package UnitTests.mock.MarketMock;
 
-public class MockMarketCashier {
+import java.util.Map;
+
+import market.BusinessOrder;
+import market.Market;
+import market.MarketCashierRole.MyCustomer;
+import interfaces.MarketCashier;
+import interfaces.MarketCustomer;
+import interfaces.MarketEmployee;
+import UnitTests.mock.Mock;
+
+public class MockMarketCashier extends Mock implements MarketCashier{
+
+	String name;
+	public MarketCustomer customer;
+	
+	
+	public MockMarketCashier(String n){
+		name = n;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	@Override
+	public boolean canLeave() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void msgServiceCustomer(MarketCustomer c,
+			Map<String, Integer> groceries) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgFinishedComputing(MyCustomer mc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgCustomerPayment(MarketCustomer c, int payment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgHereIsBusinessPayment(int payment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgCalculateInvoice(BusinessOrder order, MarketEmployee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean YouAreDoneWithShift() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setMarket(Market m) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/UnitTests/mock/MarketMock/MockMarketEmployee.java
+++ src/UnitTests/mock/MarketMock/MockMarketEmployee.java
@@ -1,5 +1,48 @@
 package UnitTests.mock.MarketMock;
 
-public class MockMarketEmployee {
+import java.util.Map;
+
+import market.BusinessOrder;
+import interfaces.MarketCustomer;
+import interfaces.MarketEmployee;
+import UnitTests.mock.Mock;
+
+public class MockMarketEmployee extends Mock implements MarketEmployee{
+
+	String name;
+	public MarketCustomer customer;
+	
+	public MockMarketEmployee(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	@Override
+	public boolean canLeave() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void msgGetItemsForCustomer(MarketCustomer c,
+			Map<String, Integer> orderList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgGetThis(BusinessOrder order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgGiveInvoice(int invoice, BusinessOrder order) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
