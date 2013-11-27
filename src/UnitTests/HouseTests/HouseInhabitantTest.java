dd5a4d4c5a56283f9b6d65ecd8e7b987f9aa4594
Linda Xu <lindaxu@usc.edu>
2013-11-26 23:51:42 -0800

    Re-added house test

--- src/UnitTests/HouseTests/HouseInhabitantTest.java
+++ src/UnitTests/HouseTests/HouseInhabitantTest.java
@@ -1,16 +1,54 @@
 package UnitTests.HouseTests;
 
+import person.PersonAgent;
 import house.InhabitantRole;
+import house.gui.InhabitantGui;
 import junit.framework.TestCase;
 
 public class HouseInhabitantTest extends TestCase {
 	InhabitantRole i;
+	InhabitantGui g;
+	PersonAgent p;
 	
 	public void setUp() throws Exception{
 		i=new InhabitantRole();
+		g=new InhabitantGui(i);
+		i.setGui(g);
+		p=new PersonAgent("MockPerson", null);
+		i.self=p;
 	}
-	public void msgTest(){
+	
+	
+	public void testParallelMsgAndScheduer(){
+		assertEquals("foodReady should be false, it is not",i.foodReady,false);
+		assertEquals("wantEat should be false, it is not",i.wantEat,false);
+		assertEquals("wantLeave should be false, it is not",i.wantLeave,false);
+		assertEquals("wantSleep should be false it is not",i.wantSleep,false);
+		assertTrue("scheduler should return false, it is not",i.pickAndExecuteAnAction()==false);
+
+		//send messages
+		i.msgFoodReady();
+		assertEquals("foodReady should be true, it is not",i.foodReady,true);
+
+		i.msgGotHungry();
+		assertEquals("wantEat should be true, it is not",i.wantEat,true);
+
+		i.msgLeaveHouse();
+		assertEquals("wantLeave should be true, it is not",i.wantLeave,true);
+
+		i.msgTired();
+		assertEquals("wantSleep should be true, it is not",i.wantSleep,true);
 		
-		
+		//run the schduler
+
+		//assertTrue("scheduler should return true, it is not",i.pickAndExecuteAnAction()==true);
+//		
+//		assertEquals("scheduler should return true, it is not",i.pickAndExecuteAnAction(),true);
+//		assertEquals("scheduler should return true, it is not",i.pickAndExecuteAnAction(),true);
+//		assertEquals("scheduler should return true, it is not",i.pickAndExecuteAnAction(),true);
+//		assertEquals("scheduler should return false, it is not",i.pickAndExecuteAnAction(),false);
+
 	}
+	
+	//public void Test
 }
