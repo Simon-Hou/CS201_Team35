b521edf67a8524515af6122836aa2e44b1828386
Gabriel Mel de Fontenay <meldefon@usc.edu>
2013-12-03 12:21:20 -0800

    Copied and pasted in the guiMoveFromCurrentLocTo() method and changed a few variable names around to make it compile. Next, I have to add in a move() method in the personGui.

--- src/person/PersonAgent.java
+++ src/person/PersonAgent.java
@@ -46,6 +46,8 @@ import java.util.concurrent.Semaphore;
 
 import javax.swing.ImageIcon;
 
+import cityGui.test.AStarTraversalPerson;
+import astar.*;
 import cityGui.test.PersonGui;
 import public_Object.Food;
 import role.Role;
@@ -111,7 +113,13 @@ public class PersonAgent extends Agent implements Person {
 	public MarketCustomerRole marketRole;
 	public InhabitantRole inhabitantRole;
 	public CustomerRole restaurantRole ;
+	
+	public AStarTraversalPerson aStar;
+    Position currentPosition; 
+    Position originalPosition;
 	private PersonGui gui;
+	
+	
 	//List<String> foodNames;
 	public Semaphore atDestination = new Semaphore(0,true);
 	public int MY_BANK = 0;
@@ -888,6 +896,104 @@ public class PersonAgent extends Agent implements Person {
 		if (role.equals("RestaurantCustomer"))
 			activeRole = restaurantRole;
 	}
+	
+	
+	
+	 //this is just a subroutine for waiter moves. It's not an "Action"
+    //itself, it is called by Actions.
+    void guiMoveFromCurrentPostionTo(Position to){
+        //System.out.println("[Gaut] " + guiWaiter.getName() + " moving from " + currentPosition.toString() + " to " + to.toString());
+
+        AStarNode aStarNode = (AStarNode)aStar.generalSearch(currentPosition, to);
+        List<Position> path = aStarNode.getPath();
+        Boolean firstStep   = true;
+        Boolean gotPermit   = true;
+
+        for (Position tmpPath: path) {
+            //The first node in the path is the current node. So skip it.
+            if (firstStep) {
+                firstStep   = false;
+                continue;
+            }
+
+            //Try and get lock for the next step.
+            int attempts    = 1;
+            gotPermit       = new Position(tmpPath.getX(), tmpPath.getY()).moveInto(aStar.getGrid());
+
+            //Did not get lock. Lets make n attempts.
+            while (!gotPermit && attempts < 3) {
+                //System.out.println("[Gaut] " + guiWaiter.getName() + " got NO permit for " + tmpPath.toString() + " on attempt " + attempts);
+
+                //Wait for 1sec and try again to get lock.
+                try { Thread.sleep(1000); }
+                catch (Exception e){}
+
+                gotPermit   = new Position(tmpPath.getX(), tmpPath.getY()).moveInto(aStar.getGrid());
+                attempts ++;
+            }
+
+            //Did not get lock after trying n attempts. So recalculating path.            
+            if (!gotPermit) {
+                //System.out.println("[Gaut] " + guiWaiter.getName() + " No Luck even after " + attempts + " attempts! Lets recalculate");
+                guiMoveFromCurrentPostionTo(to);
+                break;
+            }
+
+            //Got the required lock. Lets move.
+            //System.out.println("[Gaut] " + guiWaiter.getName() + " got permit for " + tmpPath.toString());
+            currentPosition.release(aStar.getGrid());
+            currentPosition = new Position(tmpPath.getX(), tmpPath.getY ());
+            gui.move(currentPosition.getX(), currentPosition.getY());
+        }
+        /*
+        boolean pathTaken = false;
+        while (!pathTaken) {
+            pathTaken = true;
+            //print("A* search from " + currentPosition + "to "+to);
+            AStarNode a = (AStarNode)aStar.generalSearch(currentPosition,to);
+            if (a == null) {//generally won't happen. A* will run out of space first.
+                System.out.println("no path found. What should we do?");
+                break; //dw for now
+            }
+            //dw coming. Get the table position for table 4 from the gui
+            //now we have a path. We should try to move there
+            List<Position> ps = a.getPath();
+            Do("Moving to position " + to + " via " + ps);
+            for (int i=1; i<ps.size();i++){//i=0 is where we are
+                //we will try to move to each position from where we are.
+                //this should work unless someone has moved into our way
+                //during our calculation. This could easily happen. If it
+                //does we need to recompute another A* on the fly.
+                Position next = ps.get(i);
+                if (next.moveInto(aStar.getGrid())){
+                    //tell the layout gui
+                    guiWaiter.move(next.getX(),next.getY());
+                    currentPosition.release(aStar.getGrid());
+                    currentPosition = next;
+                }
+                else {
+                    System.out.println("going to break out path-moving");
+                    pathTaken = false;
+                    break;
+                }
+            }
+        }
+        */
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
