a43f54f57ef288e9b17e6009d6c292daab3c1beb
Gabriel Mel de Fontenay <meldefon@usc.edu>
2013-12-05 18:20:34 -0800

    Committing here so I can go back if needs be. I'm going to start getting rid of stuff now

--- src/cityGui/CityRestaurantCardGabe.java
+++ src/cityGui/CityRestaurantCardGabe.java
@@ -1,12 +1,121 @@
 package cityGui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.Timer;
+
+import restaurant.restaurantGabe.gui.CookGui;
+import restaurant.restaurantGabe.gui.CustomerGui;
+import restaurant.restaurantGabe.gui.Gui;
+import restaurant.restaurantGabe.gui.WaiterGui;
+
 public class CityRestaurantCardGabe extends CityRestaurantCard{
 
 	public CityRestaurantCardGabe(SimCityGui city) {
 		super(city);
-		// TODO Auto-generated constructor stub
+		
+		setSize(WINDOWX, WINDOWY);
+        setVisible(true);
+        
+        bufferSize = this.getSize();
+    	Timer timer = new Timer(TIMER_DELAY, this );
+    	timer.start();
 	}
 	
+
 	
+	private int WINDOWX = 650;
+	private int WINDOWY = 550;
+    private Image bufferImage;
+    private Dimension bufferSize;
+    
+    private static int TIMER_DELAY = 10;
+    
+    private static int N_TABLES = 3;
+
+    private List<Gui> guis = new ArrayList<Gui>();
+
+    
+
+	public void actionPerformed(ActionEvent e) {
+		repaint();  //Will have paintComponent called
+	}
+	
+	int[] x_table; //= {200,300,400};
+    int[] y_table;// = {250,250,250};
+    int width = 50;
+    int height = 50;
+    
+    int xGrill = 415;
+    int yGrill = 125;
+    
+    int xPlate = 415;
+    int yPlate = 55;
+    
+    int xFridge = 500;
+    int yFridge = 85;
+    
+    int wFridge = 30;
+    int hFridge = 30;
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        
+        //g2.setClip(0,0,WINDOWX,WINDOWY);
+        //Clear the screen by painting a rectangle the size of the frame
+        g2.setColor(getBackground());
+        g2.fillRect(0, 0, WINDOWX, WINDOWY );
+
+        //Grills
+        g2.setColor(Color.PINK);
+        g2.fillRect(xPlate,yPlate, 250, 20);
+        g2.fillRect(xGrill,yGrill, 250, 20);
+        
+        //Fridge
+        g2.setColor(Color.blue);
+        g2.fillRect(xFridge, yFridge, wFridge, hFridge);
+        
+        
+        //Here is the table
+        g2.setColor(Color.ORANGE);
+       
+        for(int i = 0;i<N_TABLES;++i){
+        	g2.fillRect(x_table[i],y_table[i],width, height);//200 and 250 need to be table params
+        }
+
+        for(Gui gui : guis) {
+            if (gui.isPresent()) {
+                gui.updatePosition();
+            }
+        }
+
+        for(Gui gui : guis) {
+            if (gui.isPresent()) {
+                gui.draw(g2);
+            }
+        }
+    }
+
+    public void addGui(CustomerGui gui) {
+        guis.add(gui);
+    }
+
+    public void addGui(WaiterGui gui) {
+        guis.add(gui);
+    }
+    
+    public void addGui(CookGui gui){
+    	guis.add(gui);
+    }
+
+
 
 }
--- src/cityGui/CityRestaurantGabe.java
+++ src/cityGui/CityRestaurantGabe.java
@@ -1,5 +1,26 @@
 package cityGui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import restaurant.restaurantGabe.CashierRole;
+import restaurant.restaurantGabe.CookRole;
+import restaurant.restaurantGabe.CustomerRole;
+import restaurant.restaurantGabe.HostRole;
+import restaurant.restaurantGabe.gui.CookGui;
+import restaurant.restaurantGabe.gui.CustomerGui;
+import restaurant.restaurantGabe.gui.ListPanel;
+//import restaurant.restaurantGabe.gui.MarketRole;
+import restaurant.restaurantGabe.gui.RestaurantGui;
+import restaurant.restaurantGabe.gui.WaiterPanel;
+
 public class CityRestaurantGabe extends CityRestaurant{
 
 	public CityRestaurantGabe(int x, int y) {
@@ -7,6 +28,172 @@ public class CityRestaurantGabe extends CityRestaurant{
 		
 	}
 
+    //Host, cook, waiters, customers, markets, cashier that must be hacked in
+	public CookRole cook = new CookRole("Cook",0);
+    public HostRole host = new HostRole("Host");
+    public CashierRole cashier = new CashierRole("Cashier");
+    private int NUM_MARKETS = 3;
+    public List<MarketRole> Markets = new ArrayList<MarketRole>();
+    /*public MarketAgent m1 = new MarketAgent("Joel");
+    public MarketAgent m2 = new MarketAgent("Bill");
+    public MarketAgent m3 = new MarketAgent("Bob");*/
+    
+    /*private WaiterAgent waiter1 = new WaiterAgent("John");
+    public WaiterGui waiter1Gui = new WaiterGui(waiter1);
+    private WaiterAgent waiter2 = new WaiterAgent("Martha");
+    public WaiterGui waiter2Gui = new WaiterGui(waiter2);*/
+
+    private Vector<CustomerRole> customers = new Vector<CustomerRole>();
+
+    private JPanel restLabel = new JPanel();
+    public ListPanel customerPanel = new ListPanel(this, "Customers");
+    public WaiterPanel waiterPanel = new WaiterPanel(this);
+    private JPanel group = new JPanel();
+
+    public RestaurantGui gui; //reference to main gui
+    
+    public int[] xTables;
+    public int[] yTables;
+
+    public CityRestaurantGabe(int x, int y,RestaurantGui gui) {
+    	
+    	super(x,y);
+    	
+        this.gui = gui;
+        
+        /*waiter1.setGui(waiter1Gui);
+        waiter1.setCook(cook);
+        waiter1.setHost(host);
+        host.addWaiter(waiter1);
+        
+        waiter2.setGui(waiter2Gui);
+        waiter2.setCook(cook);
+        waiter2.setHost(host);
+        host.addWaiter(waiter2);*/
+        
+        //host.hostGui.xTables = super.x_table;
+        gui.stand.setCook(cook);
+        cook.setRevolvingStand(gui.stand);
+        
+
+        //gui.animationPanel.addGui(waiter1Gui);
+        //gui.animationPanel.addGui(waiter2Gui);
+        host.startThread();
+        //waiter1.startThread();
+        //waiter2.startThread();
+        
+        cashier.startThread();
+        
+        MarketRole firstM = new MarketRole("market0",0,100,100,100);
+        firstM.setCashier(cashier);
+        firstM.startThread();
+        cook.addMarket(firstM);
+        
+        MarketRole secondM = new MarketRole("market1",100,100,100,100);
+        secondM.setCashier(cashier);
+        secondM.startThread();
+        cook.addMarket(secondM);
+        
+        MarketRole thirdM = new MarketRole("market2",100,100,100,100);
+        thirdM.setCashier(cashier);
+        thirdM.startThread();
+        cook.addMarket(thirdM);
+        
+        CookGui cGui = new CookGui(cook);
+        gui.animationPanel.addGui(cGui);
+        cook.setGui(cGui);
+        
+        cook.startThread();
+        /*for(int i = 0;i<NUM_MARKETS;++i){
+        	MarketAgent m = new MarketAgent("m"+i,100,100,100,100);
+        	Markets.add(m);
+        	cook.addMarket(m);
+        	m.startThread();
+        }*/
+        /*cook.addMarket(m1);
+        cook.addMarket(m2);
+        cook.addMarket(m3);*/
+
+        setLayout(new GridLayout(1, 2, 20, 20));
+        group.setLayout(new GridLayout(1, 2, 10, 10));
+
+        group.add(customerPanel);
+
+        initRestLabel();
+        add(restLabel);
+        add(waiterPanel);
+        add(group);
+    }
+
+    /**
+     * Sets up the restaurant label that includes the menu,
+     * and host and cook information
+     */
+    private void initRestLabel() {
+        JLabel label = new JLabel();
+        //restLabel.setLayout(new BoxLayout((Container)restLabel, BoxLayout.Y_AXIS));
+        restLabel.setLayout(new BorderLayout());
+        label.setText(
+                "<html><h3><u>Tonight's Staff</u></h3><table><tr><td>host:</td><td>" + host.getName() + "</td></tr></table><h3><u> Menu</u></h3><table><tr><td>Steak</td><td>$15.99</td></tr><tr><td>Chicken</td><td>$10.99</td></tr><tr><td>Salad</td><td>$5.99</td></tr><tr><td>Pizza</td><td>$8.99</td></tr></table><br></html>");
+
+        restLabel.setBorder(BorderFactory.createRaisedBevelBorder());
+        restLabel.add(label, BorderLayout.CENTER);
+        restLabel.add(new JLabel("               "), BorderLayout.EAST);
+        restLabel.add(new JLabel("               "), BorderLayout.WEST);
+    }
+
+    /**
+     * When a customer or waiter is clicked, this function calls
+     * updatedInfoPanel() from the main gui so that person's information
+     * will be shown
+     *
+     * @param type indicates whether the person is a customer or waiter
+     * @param name name of person
+     */
+    /*public void showInfo(String type, String name) {
+
+        if (type.equals("Customers")) {
+
+            for (int i = 0; i < customers.size(); i++) {
+                CustomerAgent temp = customers.get(i);
+                if (temp.getName() == name)
+                    gui.updateInfoPanel(temp);
+            }
+        }
+    }*/
+    
+    public void setCustHungry(String name){
+    	for (int i = 0; i < customers.size(); i++) {
+            CustomerRole temp = customers.get(i);
+            if (temp.getName() == name)
+                gui.setCustomerHungry(temp);
+        }
+    	
+    	
+    }
+
+    /**
+     * Adds a customer or waiter to the appropriate list
+     *
+     * @param type indicates whether the person is a customer or waiter (later)
+     * @param name name of person
+     */
+    public void addPerson(String type, String name) {
+
+    	if (type.equals("Customers")) {
+    		CustomerRole c = new CustomerRole(name);	
+    		CustomerGui g = new CustomerGui(c, gui);
+
+    		gui.animationPanel.addGui(g);// dw
+    		c.setHost(host);
+    		c.setGui(g);
+    		c.setCashier(cashier);
+    		customers.add(c);
+    		c.startThread();
+    	}
+    }
+	
+	
 	@Override
 	public void initializeRestaurant() {
 		// TODO Auto-generated method stub
