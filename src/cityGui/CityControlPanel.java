e2a90426f34fe50a9296bd61a541840520af8d46
Gabriel Mel de Fontenay <meldefon@usc.edu>
2013-12-06 20:52:33 -0800

    Added the scenario panel. People should be able to start scenarios from there now.

--- src/cityGui/CityControlPanel.java
+++ src/cityGui/CityControlPanel.java
@@ -59,6 +59,7 @@ public class CityControlPanel extends JPanel implements ActionListener{
 		c.gridx = 2; c.gridy = 0;
 		add(addPerson, c);
 		newScenario = new JButton("Select Scenario");
+		newScenario.addActionListener(this);
 		c.gridx = 2; c.gridy = 1;
 		add(newScenario,c);
 		
@@ -107,6 +108,11 @@ public class CityControlPanel extends JPanel implements ActionListener{
 		else if (e.getSource().equals(addMarket)) {
 			AlertLog.getInstance().logInfo(AlertTag.MARKET, this.name, "Adding New Market");
 			city.city.addObject(CityComponents.MARKET);
+		}
+		else if (e.getSource().equals(newScenario)){
+			//AlertLog.getInstance().logInfo(AlertTag., this.name, "Adding New Restaurant");
+			//System.out.println("HEREERERERRE");
+			ScenarioPanel scenarioPanel = new ScenarioPanel(this.city);
 		}
 		else if(e.getSource().equals(addPerson)) {
 			city.NewPersonCreationPanel();
