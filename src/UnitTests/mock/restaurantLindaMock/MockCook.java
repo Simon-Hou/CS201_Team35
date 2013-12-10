1f2f0fec77c6968c4ae0f42564273db8c8118e3a
Linda Xu <lindaxu@usc.edu>
2013-12-10 11:31:13 -0800

    Fixed own restaurant's cashier test, and got rid of compile errors in mocks

--- src/UnitTests/mock/restaurantLindaMock/MockCook.java
+++ src/UnitTests/mock/restaurantLindaMock/MockCook.java
@@ -2,34 +2,57 @@ package UnitTests.mock.restaurantLindaMock;
 
 import java.util.Map;
 
+import market.Market;
+import market.MarketInvoice;
+
 import UnitTests.mock.LoggedEvent;
 import UnitTests.mock.Mock;
 
 import interfaces.restaurantLinda.*;
 
 public class MockCook extends Mock implements Cook {
-	
+
 	public MockCook(String name){
 		super(name);
 	}
 	
 	@Override
+	public void msgHereIsDelivery(MarketInvoice order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgCannotFulfillOrder(Market m,
+			Map<String, Integer> unfulfillable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void depleteInventory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canLeave() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
 	public void msgHereIsOrder(Waiter w, String choice, int table) {
-		log.add(new LoggedEvent("Received message from waiter " + w.getName() + " to cook " + choice + " for table " + table));
+		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void msgCannotFulfillOrder(Market m, Map<String, Integer> items) {
-		log.add(new LoggedEvent("Received message from market " + m + " that these orders cannot be fulfilled: " + items));
-		
-	}
-
-	@Override
-	public void msgFoodShipment(Map<String, Integer> items) {
-		log.add(new LoggedEvent("Received shipment of items: " + items));
-		
-	}
+	
 
 	
 }
--- src/UnitTests/mock/restaurantLindaMock/MockHost.java
+++ src/UnitTests/mock/restaurantLindaMock/MockHost.java
@@ -11,33 +11,59 @@ public class MockHost extends Mock implements Host{
 	}
 	
 	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canLeave() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
 	public void msgIWantFood(Customer cust) {
-		log.add(new LoggedEvent("Received IWantFood from customer " + cust.getName()));
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void msgIWillWait(Customer cust, boolean wait) {
-		log.add(new LoggedEvent("Customer " + cust.getName() + " has decided to wait"));
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void msgCustomerLeaving(Waiter waiter, Customer cust, int t) {
-		log.add(new LoggedEvent("Waiter " + waiter.getName() + " says customer " + cust.getName() + " is leaving table " + t));
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void msgIWantBreak(Waiter waiter) {
-		log.add(new LoggedEvent("Received IWantBreak from waiter " + waiter.getName()));
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void msgOffBreak(Waiter waiter) {
-		log.add(new LoggedEvent("Received OffBreak from waiter " + waiter.getName()));
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void addTable(int size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addWaiter(Waiter m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
