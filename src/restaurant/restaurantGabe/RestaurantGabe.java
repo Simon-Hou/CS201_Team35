b012d727b3718447a5aa2e816b713e5ff7801a68
Gabriel Mel de Fontenay <meldefon@usc.edu>
2013-12-10 00:04:41 -0800

    Fixed a small glitch in the paying situation

--- src/restaurant/restaurantGabe/RestaurantGabe.java
+++ src/restaurant/restaurantGabe/RestaurantGabe.java
@@ -80,7 +80,7 @@ public class RestaurantGabe extends Restaurant{
 	    	
 	    	cook = new CookRole("DefaultCook");
 	    	super.cook = cook;
-	    	//cook.Markets = this.
+	    	cook.Markets = this.markets;
 		    host = new HostRole("DefaultHost");
 		    super.host = host;
 		    cashier = new CashierRole("DefaultCashier");
